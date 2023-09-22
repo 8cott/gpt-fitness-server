497dba52810857a9336796bf7464245edbac99b7
Scott <109820423+8cott@users.noreply.github.com>
2023-09-22 12:10:33 -0400

    deployment

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
+import os
 from app import create_app
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
