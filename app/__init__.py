2ce063973653b5a029dfd77170066715cd8e0ede
Scott <109820423+8cott@users.noreply.github.com>
2023-09-26 15:08:46 -0400

    deployment

--- app/__init__.py
+++ app/__init__.py
@@ -16,9 +16,17 @@ def create_app(*args, **kwargs):
 
     # Check if the app is running on Heroku
     if 'DYNO' in os.environ:
+        if not os.environ.get("DATABASE_URL"):
+            raise ValueError("Missing DATABASE_URL environment variable")
+        
         # Heroku
         openai.api_key = os.environ.get("OPENAI_API_KEY")
-        app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
+        
+        database_url = os.environ.get("DATABASE_URL")
+        if database_url.startswith("postgres://"):
+            database_url = database_url.replace("postgres://", "postgresql://", 1)
+        app.config['SQLALCHEMY_DATABASE_URI'] = database_url
+        
         app.config['JWT_SECRET_KEY'] = os.environ.get('JWT_SECRET_KEY')
     else:
         # Local Env
