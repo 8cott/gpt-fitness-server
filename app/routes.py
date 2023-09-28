62a1f0ac3cb2c80f394f2b8a5174feda836c15b2
Scott <109820423+8cott@users.noreply.github.com>
2023-09-28 17:02:52 -0400

    added temp and token log

--- app/routes.py
+++ app/routes.py
@@ -87,8 +87,12 @@ def generate_plan():
         response = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
             messages=[{"role": "system", "content": "You are a fitness assistant."},
-                      {"role": "user", "content": prompt}]
+                      {"role": "user", "content": prompt}],
+            temperature=1
         )
+
+        tokens_used = response['usage']['total_tokens']
+        print(f"Tokens used in this request: {tokens_used}")
 
         plan = response.choices[0].message["content"]
 
