0bcc357e0300ff01566f5595629663ebb805e6e7
Scott <109820423+8cott@users.noreply.github.com>
2023-10-24 13:46:29 -0400

    debugging response time

--- app/routes.py
+++ app/routes.py
@@ -81,7 +81,7 @@ def generate_plan():
         print("Generated Prompt:", prompt)
 
         response = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo-16k",
+            model="gpt-3.5-turbo",
             messages=[{"role": "system", "content": "You are a fitness assistant."},
                       {"role": "user", "content": prompt}],
             temperature=0.7,
