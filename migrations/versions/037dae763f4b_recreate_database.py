4a1c107e3261877205b630ab3f49b81ca459aca1
Scott <109820423+8cott@users.noreply.github.com>
2023-10-25 15:45:50 -0400

    recreated database

---
+++ migrations/versions/037dae763f4b_recreate_database.py
@@ -0,0 +1,24 @@
+"""Recreate Database
+
+Revision ID: 037dae763f4b
+Revises: c7974ddabf2b
+Create Date: 2023-10-25 15:45:20.440338
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '037dae763f4b'
+down_revision = 'c7974ddabf2b'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
---
+++ migrations/versions/c7974ddabf2b_migrate_changes.py
@@ -0,0 +1,24 @@
+"""Migrate changes
+
+Revision ID: c7974ddabf2b
+Revises: e90bf1bcf1f1
+Create Date: 2023-10-25 15:39:15.097142
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'c7974ddabf2b'
+down_revision = 'e90bf1bcf1f1'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
