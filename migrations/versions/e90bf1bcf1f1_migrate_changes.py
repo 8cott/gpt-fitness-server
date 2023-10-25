4c8dc26a7681d54781c39ee29ce525758f0693e3
Scott <109820423+8cott@users.noreply.github.com>
2023-10-25 15:21:02 -0400

    migration

---
+++ migrations/versions/e90bf1bcf1f1_migrate_changes.py
@@ -0,0 +1,24 @@
+"""Migrate changes
+
+Revision ID: e90bf1bcf1f1
+Revises: 
+Create Date: 2023-10-25 15:19:56.811596
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'e90bf1bcf1f1'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
